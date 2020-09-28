ed2f93d03e68641f1be684f4f4adc6e74cd07ab6
Ayato <a.b.s2121.ayt@outlook.jp>
2020-09-29 00:44:22 +0900

    Add multi-page routing

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -27,6 +27,7 @@ class BurgerBuilder extends Component {
   }
 
   componentDidMount() {
+    console.log(this.props);
     axios.get('https://react-my-burger-6b830.firebaseio.com/ingredients.json')
     .then(response => {
       this.setState({ ingredients: response.data });
@@ -91,29 +92,18 @@ class BurgerBuilder extends Component {
   }
 
   purchaseContinueHandler = () => {
-    // alert('You Continue!');
-    this.setState({ loading: true })
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Ayato Higuchi',
-        address: {
-          zipcode: '1234567',
-          country: 'japan'
-        },
-        email: 'test@test.com'
-      },
-      deliveryMethd: 'fastest'
-    };
+    const queryParams = [];
+    for (let i in this.state.ingredients) {
+      queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+    }
+    queryParams.push('price=' + this.state.totalPrice);
 
-    axios.post('/orders.json', order)
-      .then(response => {
-        this.setState({ loading: false, purchasing: false })
-      })
-      .catch(error => {
-        this.setState({ loading: false, purchasing: false })
-      });
+    const queryString = queryParams.join('&');
+
+    this.props.history.push({
+      pathname: '/checkout',
+      search: '?' + queryString
+    });
   }
 
   render() {
