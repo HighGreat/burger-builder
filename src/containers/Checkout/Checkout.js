9ab33e8e1988ce2da43da99842d9ea109ef39e2d
Ayato <a.b.s2121.ayt@outlook.jp>
2020-10-05 00:41:39 +0900

    Add redux and fix state management

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
-  state = {
-    ingredients: null,
-    price: 0
-  }
-
   componentWillMount() {
     const query = new URLSearchParams(this.props.location.search);
     const ingredients = {};
@@ -39,16 +35,24 @@ class Checkout extends Component {
     return (
       <div>
         <CheckoutSummary 
-          ingredients={this.state.ingredients} 
+          ingredients={this.props.ings} 
           checkoutCancelled={this.checkoutCancelledHandler}
           checkoutContinued={this.checkouteContinuedHandler}
         />
         <Route 
           path={this.props.match.path + '/contact-data'} 
-          render={(props) => (<ContactData ingredients={this.state.ingredients} price={this.state.price} {...props} />)} />
+          component={ContactData}
+        />
       </div>
     );
   }
 }
 
-export default Checkout
+const mapStateToProps = state => {
+  return {
+    ings: state.ingredients,
+    price: state.totalPrice
+  };
+}
+
+export default connect(mapStateToProps)(Checkout);
